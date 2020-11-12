a86d48db23a071752fa1fc4159e2f7e44b070875
@abiydv <abiydv@outlook.com>
2020-11-12 10:47:27 +0000

    format

--- main.tf
+++ main.tf
@@ -15,20 +15,20 @@ resource "fastly_service_v1" "meta" {
   name = "meta"
 
   domain {
-      name = format("meta.%s-%s",random_pet.domain.id,local.domain)
+    name = format("meta.%s-%s", random_pet.domain.id, local.domain)
   }
 
   backend {
-      name    = "httpbin.org"
-      address = "httpbin.org"
-      port    = 443
+    name    = "httpbin.org"
+    address = "httpbin.org"
+    port    = 443
   }
 
   dynamic "snippet" {
-    for_each = [ for s in ["recv","error"] : { name = s }]
+    for_each = [for s in ["recv", "error"] : { name = s }]
     content {
-      name = snippet.value.name
-      type = snippet.value.name
+      name    = snippet.value.name
+      type    = snippet.value.name
       content = file("${path.module}/vcl/${snippet.value.name}.vcl")
     }
   }
@@ -38,10 +38,10 @@ resource "fastly_service_v1" "meta" {
 
 
 output "service_domain" {
-    value = tolist(fastly_service_v1.meta.domain)[0].name
+  value = tolist(fastly_service_v1.meta.domain)[0].name
 }
 
 output "shared_service_domain" {
-    value = format("%s.global.prod.fastly.net",tolist(fastly_service_v1.meta.domain)[0].name)
+  value = format("%s.global.prod.fastly.net", tolist(fastly_service_v1.meta.domain)[0].name)
 }
 
